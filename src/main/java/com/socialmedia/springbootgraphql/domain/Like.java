676f5c2d7ec523db6d7fb280ab3e7cc5b0e89808
Nebyat Tibebu <nebyatweb@users.noreply.github.com>
2022-09-14 09:16:38 -0700

    bug-fix: change table name to likes

--- src/main/java/com/socialmedia/springbootgraphql/domain/Like.java
+++ src/main/java/com/socialmedia/springbootgraphql/domain/Like.java
@@ -6,9 +6,11 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "likes")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
