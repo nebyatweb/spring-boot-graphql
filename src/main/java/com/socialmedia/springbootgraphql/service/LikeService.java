9adb50db5f0ebc28ea48d773903c3b450b60432e
Nebyat Tibebu <nebyatweb@users.noreply.github.com>
2022-09-14 16:57:39 -0700

    add method definitions

--- src/main/java/com/socialmedia/springbootgraphql/service/LikeService.java
+++ src/main/java/com/socialmedia/springbootgraphql/service/LikeService.java
@@ -1,4 +1,12 @@
 package com.socialmedia.springbootgraphql.service;
 
+import com.socialmedia.springbootgraphql.domain.Like;
+
+import java.util.List;
+
 public interface LikeService {
+    Like createLike(Like like);
+    Like removeLike(long likeId);
+    Like getLikeById(long likeId);
+    List<Like> getAllLikes();
 }
