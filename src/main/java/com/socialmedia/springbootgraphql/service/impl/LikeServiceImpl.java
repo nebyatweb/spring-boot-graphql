ebd439fb7b2e1edafadcbb671fe53e297b95e8e4
Nebyat Tibebu <nebyatweb@users.noreply.github.com>
2022-09-14 17:14:25 -0700

    add CRUD operations

--- src/main/java/com/socialmedia/springbootgraphql/service/impl/LikeServiceImpl.java
+++ src/main/java/com/socialmedia/springbootgraphql/service/impl/LikeServiceImpl.java
@@ -1,2 +1,44 @@
-package com.socialmedia.springbootgraphql.service.impl;public class LikeServiceImpl {
+package com.socialmedia.springbootgraphql.service.impl;
+
+import com.socialmedia.springbootgraphql.domain.Like;
+import com.socialmedia.springbootgraphql.exception.LikeNotFoundException;
+import com.socialmedia.springbootgraphql.repository.LikeRepository;
+import com.socialmedia.springbootgraphql.service.LikeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LikeServiceImpl implements LikeService {
+    private LikeRepository likeRepository;
+
+    public LikeServiceImpl(LikeRepository likeRepository) {
+        this.likeRepository = likeRepository;
+    }
+
+    @Override
+    public Like createLike(Like like) {
+        return likeRepository.save(like);
+    }
+
+    @Override
+    public void removeLike(long likeId) {
+        if(likeRepository.findById(likeId).isPresent())
+            likeRepository.deleteById(likeId);
+        else
+            throw new RuntimeException("Cannot find like with id " + likeId);
+    }
+
+    @Override
+    public Like getLikeById(long likeId) {
+        if(likeRepository.findById(likeId).isPresent())
+            return likeRepository.findById(likeId).get();
+        else
+            throw new LikeNotFoundException("Post with postId "+likeId+" does not exist!");
+    }
+
+    @Override
+    public List<Like> getAllLikes() {
+        return likeRepository.findAll();
+    }
 }
